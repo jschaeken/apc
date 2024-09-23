392c98c2dfdb7eb6af17b2a39e974d2df5b23d6e
Jacques <jacques@essencesoftwaredevelopment.com>
2024-09-23 15:46:39 +0200

    Logo changed, push notifications now can contain images

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Jacques Schaeken on 23/09/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
